3583d6b5330d89c00da949a431e8ce5e57b06288
Utsav Singh Tomar <utsav19october@gmail.com>
2023-03-30 10:48:34 +0530

    officer schema and auth

--- client/models/Officer.js
+++ client/models/Officer.js
@@ -42,24 +42,24 @@ const OfficerSchema = new mongoose.Schema({
     }
 })
 
-UserSchema.pre('save', async function () {
+OfficerSchema.pre('save', async function () {
 
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt)
 
 })
 
-UserSchema.methods.getName = function () {
+OfficerSchema.methods.getName = function () {
     return this.name
 }
 
-UserSchema.methods.createJWT = function () {
+OfficerSchema.methods.createJWT = function () {
     return jwt.sign({ userId: this._id, name: this.name }, process.env.JWT_SECRET, {
         expiresIn: process.env.JWT_EXPIRES_IN
     })
 }
 
-UserSchema.methods.comparePassword = async function (candidatePassword) {
+OfficerSchema.methods.comparePassword = async function (candidatePassword) {
     const isMatch = await bcrypt.compare(candidatePassword, this.password)
     return isMatch
 }

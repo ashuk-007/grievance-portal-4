de29fffd03caf190febfefd1f9404bfd2a52ee39
Vishesh Vijayvargiya <vijay.vishu03@gmail.com>
2023-04-08 03:06:05 +0530

    adminintegration

--- client/src/Components/AdminProfile.jsx
+++ client/src/Components/AdminProfile.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 import pfp from "../Images/pfp.png";
+import axios from "axios";
 export default function AdminProfile(props){
-  const token=localStorage.getItem("token");
+ const token = localStorage.getItem("token");
+ let config = {
+   method: "get",
+   maxBodyLength: Infinity,
+   url: "http://localhost:3000/api/v1/officer",
+   headers: {
+     Authorization: `Bearer ${token}`,
+   },
+ };
+ const [officerData, setOfficerData] = React.useState({});
+ React.useEffect(() => {
+   axios
+     .request(config)
+     .then((response) => {
+       console.log(JSON.stringify(response.data));
+       setOfficerData(response.data.officer);
+     })
+     .catch((error) => {
+       console.log(error);
+       alert("Error Occured");
+     });
+ }, []);
+
     return (
       <div
         className={
@@ -14,22 +37,26 @@ export default function AdminProfile(props){
         <h1 className="text-center text-3xl  md:text-7xl font-semibold">YOUR PROFILE</h1>
         <div className="name-input flex justify-center items-center mt-12 md:mt-24">
           <h4 className="text-xl md:text-3xl font-bold ml-6">Name:</h4>
-          <h4 className="text-xl md:text-3xl ml-4 md:ml-8">{props.uName}</h4>
+          <h4 className="text-xl md:text-3xl ml-4 md:ml-8">{officerData.name}</h4>
         </div>
-        <div className="name-input flex justify-center items-center mt-6 md:mt-12">
+        <div className="name-input flex justify-center items-center mt-6 md:mt-10">
           <h4 className="text-xl md:text-3xl font-bold ml-6">Email:</h4>
-          <h4 className="text-xl md:text-3xl ml-4 md:ml-8">{props.mail}</h4>
+          <h4 className="text-xl md:text-3xl ml-4 md:ml-8">{officerData.email}</h4>
         </div>
-        <div className="name-input flex justify-center items-center mt-6 md:mt-12">
-          <h4 className="text-xl md:text-3xl font-bold ml-6">Phone Number:</h4>
-          <h4 className="text-xl md:text-3xl ml-4 md:ml-8">{props.contact}</h4>
+        <div className="name-input flex justify-center items-center mt-6 md:mt-10">
+          <h4 className="text-xl md:text-3xl font-bold ml-6">Level:</h4>
+          <h4 className="text-xl md:text-3xl ml-4 md:ml-8">{officerData.level}</h4>
         </div>
-        <div className="name-input flex-row justify-center items-center mt-6 md:mt-12">
+        <div className="name-input flex justify-center items-center mt-6 md:mt-10">
+          <h4 className="text-xl md:text-3xl font-bold ml-6">Department:</h4>
+          <h4 className="text-xl md:text-3xl ml-4 md:ml-8">{officerData.department}</h4>
+        </div>
+        <div className="name-input flex justify-center items-center mt-6 md:mt-10">
           <h4 className="text-xl md:text-3xl text-center font-bold ml-6">
-            Address:
+            District:
           </h4>
           <h4 className="text-xl md:text-3xl text-center ml-4 md:ml-8">
-            {props.address}
+            {officerData.district}
           </h4>
         </div>
       </div>
--- client/src/Components/GrievanceStatus.jsx
+++ client/src/Components/GrievanceStatus.jsx
@@ -1,6 +1,38 @@
 import React from "react";
-
+import axios from "axios";
 export default function GrievanceStatus(props) {
+  const token = localStorage.getItem("token");
+  let config = {
+    method: "get",
+    maxBodyLength: Infinity,
+    url: "http://localhost:3000/api/v1/tasks",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  const [complaints, setComplaints] = React.useState([]);
+  React.useEffect(() => {
+    axios
+      .request(config)
+      .then((response) => {
+        console.log(JSON.stringify(response.data));
+        setComplaints(response.data.tasks);
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("Error Occured");
+      });
+  }, []);
+  const complaintData = complaints.map((complaint) => <tr>
+            <td className="border px-4 py-1 whitespace-nowrap">{complaint._id}</td>
+            <td className="border px-4 py-1 whitespace-nowrap">{complaint.createdAt}</td>
+            <td className="border px-8 py-2 whitespace-nowrap">{complaint.department}</td>
+            <td className="border px-8 py-2 whitespace-nowrap">{complaint.subject}</td>
+            <td className="border px-8 py-2 whitespace-nowrap">{complaint.description}</td>
+            <td className="border px-8 py-2 whitespace-nowrap">{complaint.status}</td>
+            <td className="border px-8 py-2 whitespace-nowrap">{complaint.updatedAt?complaint.updatedAt:"not updated"}</td>
+            <td className="border px-3 py-2 whitespace-nowrap">reminder</td> 
+          </tr>)
   return (
     <div
       class={
@@ -9,46 +41,27 @@ export default function GrievanceStatus(props) {
           : "hidden"
       }
     >
-      <h1 className="text-center font-bold text-3xl md:text-6xl">GRIEVANCES STATUS</h1>
+      <h1 className="text-center font-bold text-3xl md:text-6xl">
+        GRIEVANCES STATUS
+      </h1>
       {/* // index.html */}
       <div className="px-5 py-5  overflow-auto rounded-lg shadow mt-16 md:mt-36">
         <table className="rounded-lg shadow bg-while w-full left-5">
           <tr>
-            <th className="bg-blue-100 border text-left px-8 py-2">Date</th>
+            <th className="bg-blue-100 border text-left px-8 py-2">ID</th>
+            <th className="bg-blue-100 border text-left px-8 py-2">Time</th>
             <th className="bg-blue-100 border text-left px-8 py-2">
               Department
             </th>
-            <th className="bg-blue-100 border text-left px-20 py-2">
-              Grievance
+            <th className="bg-blue-100 border text-left px-20 py-2">Subject</th>
+            <th className="bg-blue-100 border text-left px-8 py-2">
+              Description
             </th>
             <th className="bg-blue-100 border text-left px-8 py-2">Status</th>
+            <th className="bg-blue-100 border text-left px-8 py-2">UpdatedTime</th>
             <th className="bg-blue-100 border text-left px-3 py-2">Reminder</th>
           </tr>
-          <tr>
-            <td className="border px-4 py-1 whitespace-nowrap">12-12-22</td>
-            <td className="border px-8 py-2 whitespace-nowrap">water</td>
-            <td className="border px-8 py-2 whitespace-nowrap">Dante </td>
-            <td className="border px-8 py-2 whitespace-nowrap">Italy</td>
-            <td className="border px-8 py-2 whitespace-nowrap">send</td>
-          </tr>
-          <tr>
-            <td className="border px-4 py-1 whitespace-nowrap">12-12-22</td>
-            <td className="border px-8 py-2 whitespace-nowrap">water</td>
-            <td className="border px-8 py-2 whitespace-nowrap">
-              Neal Garrison
-            </td>
-            <td className="border px-8 py-2 whitespace-nowrap">Spain</td>
-            <td className="border px-8 py-2 whitespace-nowrap">send</td>
-          </tr>
-          <tr>
-            <td className="border px-4 py-1 whitespace-nowrap">12-12-22</td>
-            <td className="border px-8 py-2 whitespace-nowrap">water</td>
-            <td className="border px-8 py-2 whitespace-nowrap">
-              Maggie O'Neill
-            </td>
-            <td className="border px-8 py-2 whitespace-nowrap">Austria</td>
-            <td className="border px-8 py-2 whitespace-nowrap">send</td>
-          </tr>
+          {complaintData}
         </table>
       </div>
     </div>

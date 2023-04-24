f33c6f8e09aaefd2019e34222588a331053b0fb3
Utsav Singh Tomar <utsav19october@gmail.com>
2023-04-24 19:04:11 +0530

    reopen

--- server/routes/complaints.js
+++ server/routes/complaints.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const { getAllComplaints, getComplaint, createComplaint, deleteComplaint, sendReminder } = require('../controllers/complaints')
+const { getAllComplaints, getComplaint, createComplaint, deleteComplaint, sendReminder, reopenTask } = require('../controllers/complaints')
 // const { roleAuthenticationMiddleware } = require('../middleware/roleAuthentication')
 
 router.route('/').get(getAllComplaints).post(createComplaint)
-router.route('/:id').get(getComplaint).delete(deleteComplaint)
+router.route('/:id').get(getComplaint).delete(deleteComplaint).patch(reopenTask)
 router.route('/reminder/:id').patch(sendReminder);
 
 

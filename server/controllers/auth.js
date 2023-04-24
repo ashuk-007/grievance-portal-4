43ee8322e9879f6c2f90eab1e75ce3ced0d9230c
Utsav Singh Tomar <utsav19october@gmail.com>
2023-04-24 17:50:44 +0530

    forgot password

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,7 @@
 const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes')
 const { BadRequestError, UnauthenticatedError } = require('../errors/')
+var nodemailer = require('nodemailer');
 // const Blacklist  = require('../models/Blacklist')
 
 const register = async (req, res) => {
@@ -15,22 +16,6 @@ const register = async (req, res) => {
     }
 
 }
-
-
-
-// const logout = async(req, res) => {
-//     const token = req.headers.authorization.split(' ')[1];
-//     const checkIfBlacklisted = await Blacklist.findOne({token: token})
-//     if (checkIfBlacklisted){
-//         res.status(204).json({msg:'Session already logged out'})
-//     }
-//     const newBlacklist = new Blacklist({
-//         token:token,
-//     });
-//     await newBlacklist.save(); 
-//     res.status(200).json({msg:'Logged out successfully!'});
-// }
-
 
 const login = async (req, res) => {
     //send the response
@@ -55,5 +40,81 @@ const login = async (req, res) => {
 
 }
 
-module.exports = { login, register }
+const forgotPassword = async (req, res) => {
+    const { email } = req.body;
+  
+    // Check if user exists with the provided email
+    const user = await User.findOne({ email });
+    if (!user) {
+      throw new BadRequestError('User not found');
+    }
+  
+    // Generate a password reset token and expiry time
+    const resetToken = user.generatePasswordResetToken();
+    user.passwordResetToken = resetToken;
+    user.passwordResetExpires = Date.now() + 3600000; // 1 hour
+  
+    // Save the updated user with password reset token and expiry time
+    await user.save();
+  
+    // Create a nodemailer transporter object
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+          user: 'grievanceportal25@gmail.com',
+          pass: 'xtovkwiqixktkimo',
+        },
+    });
+  
+    // Compose the email message with the password reset link
+    const resetUrl = `${req.protocol}://${req.headers.host}/api/v1/auth/reset-password/${resetToken}`;
+    const message = {
+      from: 'grievanceportal25@gmail.com', // Your Gmail email address
+      to: email,
+      subject: 'Password reset request',
+      html: `
+        <p>Hello,</p>
+        <p>You have requested a password reset for your account. Please click on the following link to reset your password:</p>
+        <a href="${resetUrl}">${resetUrl}</a>
+        <p>This link will expire in 1 hour.</p>
+        <p>If you did not request this reset, please ignore this email and your password will remain unchanged.</p>
+        <p>Thank you.</p>
+      `,
+    };
+  
+    // Send the email message
+    transporter.sendMail(message, (err, info) => {
+      if (err) {
+        console.log(err);
+        throw new Error('Failed to send password reset email');
+      } else {
+        console.log(info);
+        res.status(StatusCodes.OK).json({ message: 'Password reset email sent' });
+      }
+    });
+  };
+
+const resetPassword = async (req, res) => {
+    try {
+      const { password } = req.body;
+      const {resetToken} = req.params;
+      const user = await User.findOne({ passwordResetToken: resetToken, passwordResetExpires: { $gt: Date.now() } });
+      if (!user) {
+        throw new BadRequestError('Invalid or expired reset token');
+      }
+      user.password = password;
+      user.passwordResetToken = null;
+      user.passwordResetExpires = null;
+      await user.save();
+      res.status(200).json({ message: 'Password reset successfully' });
+    } catch (error) {
+      console.error(error);
+      res.status(error.statusCode || 500).json({ error: error.message || 'Something went wrong' });
+    }
+  };
+
+
+module.exports = { login, register, forgotPassword, resetPassword }
 

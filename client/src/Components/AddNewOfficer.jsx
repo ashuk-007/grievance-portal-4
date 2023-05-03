c39c93b1a874c6950ebd6c1e39228aadb7c56ed4
Vishesh Vijayvargiya <vijay.vishu03@gmail.com>
2023-05-03 13:25:54 +0530

    adminpages

---
+++ client/src/Components/AddNewOfficer.jsx
@@ -0,0 +1,99 @@
+import React from 'react'
+import axios from "axios";
+import Loading from "./Loading";
+
+export default function AddNewOfficer(props) {
+     const [data, setData] = React.useState();
+      const [loading, setLoading] = React.useState(false);
+      function checkLogin() {
+        if (!token) {
+          navigate("/userAdminLogin");
+        }
+      }
+  return (
+    <>
+      {checkLogin}
+
+      <div
+        className={
+          props.visible == "new"
+            ? "p-4 update-profile-content dashboard w-full md:w-3/4 h-100  pt-10"
+            : "hidden"
+        }
+      >
+        <h1 className="md:text-7xl text-4xl font-bold text-black capitalize dark:text-black py-4 text-center px-20  ">
+          ADD NEW OFFICER
+        </h1>
+        {/* <div className="flex justify-center">
+          <form className="border-2 shadow-2xl p-6 mt-8 w-11/12 md:w-4/6 rounded-xl">
+            <div className="grid grid-cols-1 gap-6 sm:grid-cols-1 px-4">
+              <div>
+                <label className="text-black dark:text-gray-200" htmlFor="Dept">
+                  Department
+                </label>
+                <select
+                  className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+                  onChange={(e) => handleChange(e)}
+                  name="department"
+                  value={data.department}
+                >
+                  <option>--SELECT--</option>
+                  <option value="Education">Education</option>
+                  <option value="Health">Health</option>
+                  <option value="Transport">Transport</option>
+                  <option value="Pension">Pension</option>
+                  <option value="other">other</option>
+                </select>
+              </div>
+
+              <div>
+                <label
+                  className="text-black dark:text-gray-200"
+                  htmlFor="Subject"
+                >
+                  Subject
+                </label>
+                <input
+                  id="Subject"
+                  name="subject"
+                  type="text"
+                  className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+                  onChange={(e) => handleChange(e)}
+                />
+                {loading == true && <Loading />}
+              </div>
+              <div>
+                <label
+                  className="text-black dark:text-gray-200"
+                  htmlFor="passwordConfirmation"
+                >
+                  Write your complaint
+                </label>
+                <textarea
+                  id="textarea"
+                  name="description"
+                  type="textarea"
+                  className="block w-full px- py-8 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+                  onChange={(e) => handleChange(e)}
+                ></textarea>
+              </div>
+            </div>
+
+            <div className="flex justify-around mt-6 " color="black">
+              <button
+                type="submit"
+                className="bg-light-green p-2 md:p-3 w-32 rounded-3xl text-white text-xl"
+                onClick={(e) => handleSubmit(e)}
+              >
+                Submit
+              </button>
+            </div>
+            <div className="text-light-green flex justify-center mt-3">
+              {submit ? "Your complaint has been filed" : ""}
+            </div>
+          </form>
+        </div> */}
+      </div>
+    </>
+  );
+}
--- client/src/Components/ViewOfficerDetails.jsx
+++ client/src/Components/ViewOfficerDetails.jsx
@@ -4,149 +4,30 @@ import moment from "moment";
 import Modal from "./Modal";
 import Loading from "./Loading";
 export default function MyGrievance(props) {
-  const token = localStorage.getItem("token");
-
-  let config = {
-    method: "get",
-    maxBodyLength: Infinity,
-    url: "http://localhost:3000/api/v1/complaints",
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  };
-  const [grievances, setGrievances] = React.useState([]);
- grievances.sort(function (a, b) {
-   return a.status > b.status ? 1 : b.status > a.status ? -1 : 0;
- });
-  React.useEffect(() => {
-    axios
-      .request(config)
-      .then((response) => {
-        console.log(JSON.stringify(response.data));
-        setGrievances(response.data.complaints);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
+  const [officerDetails, setOfficerDetails] = React.useState([]);
+ 
   const [isVisible, setIsVisible] = React.useState(false);
-  const [actionHistory, setActionHistory] = React.useState();
-  function handleAction(grievance){
-    setActionHistory(grievance.actionHistory)
-    setIsVisible((prev) => !prev)
-  }
-  function handleReopen(id){
-    let config = {
-      method: "patch",
-      maxBodyLength: Infinity,
-      url: `http://localhost:3000/api/v1/complaints/${id}`,
-      headers: {
-        Authorization:
-          `Bearer ${token}`,
-      },
-    };
 
-    axios
-      .request(config)
-      .then((response) => {
-        console.log(JSON.stringify(response.data));
-        alert("Reopened Successfully");
-        window.location.reload(true);
-      })
-      .catch((error) => {
-        console.log(error);
-        alert("Error Occured")
-      });
-  }
-  function handleReminder(id){
-    setLoading(true)
-    let config2 = {
-      method: "patch",
-      maxBodyLength: Infinity,
-      url: `http://localhost:3000/api/v1/complaints/reminder/${id}`,
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    };
-    axios
-      .request(config2)
-      .then((response) => {
-         setLoading(false);
-        alert("Reminder Sent Successfully")
-        window.location.reload(true);
-      })
-      .catch((error) => {
-        console.log(error);
-        alert("Error Occured")
-      });
-  }
+
   const [loading, setLoading] = React.useState(false);
-  const [rating,setRating]=React.useState(-1);
-
-  function changeRating(e){
-    setRating(e.target.value)
-    
-  }
-console.log(rating);
-
-  function handleRating(id){
-    if(rating==-1){
-      alert("Rating not given")
-    }
-    else{
-      setLoading(true)
-      
-      let config3 = {
-        method: "patch",
-        maxBodyLength: Infinity,
-        url: `http://localhost:3000/api/v1/complaints/rateOfficer/${id}`,
-        headers: {
-          "Content-Type": "application/json",
-          Authorization:
-            `Bearer ${token}`,
-        },
-        data:{"rating":rating},
-      };
-
-      axios
-        .request(config3)
-        .then((response) => {
-          console.log(JSON.stringify(response.data));
-          alert("Rating submitted")
-          window.location.reload(true)
-          setLoading(false);
-        })
-        .catch((error) => {
-          console.log(error);
-          alert(error)
-        });
-    }
-  }
+  const grievances=[]
   const grievanceData = grievances.map((grievance) => (
     <Fragment>
-      <tr
-        class={
-          grievance.status != "resolved"
-            ? grievance.status == "pending"
-              ? "bg-red"
-              : "bg-yellow"
-            : "bg-green"
-        }
-      >
+      <tr>
         <td class="px-4 py-3 text-ms font-semibold border">
-          {moment(grievance.createdAt).format("DD/MM/YYYY HH:mm")}
+          {/* {moment(grievance.createdAt).format("DD/MM/YYYY HH:mm")} */}
         </td>
         <td class="px-4 py-3 text-ms font-semibold border">
-          {grievance.department}
+          {/* {grievance.department} */}
         </td>
         <td class="px-4 py-3 text-ms font-semibold border">
-          {grievance.subject}
+          {/* {grievance.subject} */}
         </td>
         <td class="px-4 py-3 text-ms font-semibold border">
-          {grievance.status}
+          {/* {grievance.status} */}
         </td>
         <td class="px-4 py-3 text-ms font-semibold border">
-          {grievance.status != "resolved" ? (
+          {/* {grievance.status != "resolved" ? (
             grievance.lastRemindedAt == null ||
             new Date().getDate() -
               new Date(grievance.lastRemindedAt).getDate() >
@@ -169,10 +50,10 @@ console.log(rating);
             )
           ) : (
             "resolved"
-          )}
+          )} */}
         </td>
         <td class="px-4 py-3 text-ms font-semibold border">
-          <button
+          {/* <button
             className="bg-light-green hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
             onClick={() => handleAction(grievance)}
           >
@@ -182,20 +63,20 @@ console.log(rating);
             visible={isVisible}
             setVisible={setIsVisible}
             data={actionHistory}
-          />
+          /> */}
         </td>
         <td class="px-4 py-3 text-ms font-semibold border">
-          {grievance.status == "resolved" && grievance.isRated==false && (
+          {/* {grievance.status == "resolved" && grievance.isRated == false && (
             <button
               className="bg-light-green hover:bg-blue-700 text-white font-bold py-2 px-4 rounded  "
               onClick={() => handleReopen(grievance._id)}
             >
               Reopen
             </button>
-          )}
+          )} */}
         </td>
         <td class="px-4 py-3 text-ms font-semibold border">
-          {grievance.status == "resolved" && grievance.isRated==false? (
+          {/* {grievance.status == "resolved" && grievance.isRated == false ? (
             <form className="flex justify-evenly">
               <select
                 name="rating"
@@ -213,33 +94,37 @@ console.log(rating);
               </select>
               <button
                 className="bg-light-green hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4 "
-                onClick={()=>handleRating(grievance._id)}
+                onClick={() => handleRating(grievance._id)}
               >
                 Rate
               </button>
             </form>
-          ):(grievance.status=="resolved"?"Thank you for your feedback":"We are working on it")}
+          ) : grievance.status == "resolved" ? (
+            "Thank you for your feedback"
+          ) : (
+            "We are working on it"
+          )} */}
         </td>
       </tr>
     </Fragment>
   ));
-function checkLogin() {
-  if (!token) {
-    navigate("/userAdminLogin");
+  function checkLogin() {
+    if (!token) {
+      navigate("/userAdminLogin");
+    }
   }
-}
   return (
     <>
       {checkLogin}
       <div
         className={
           props.visible == "view"
-            ? "p-4 view-grievance dashboard w-full md:w-3/4 h-100  pt-10  "
+            ? "p-4 view-officer-details dashboard w-full md:w-3/4 h-100  pt-10  "
             : "hidden"
         }
       >
         <h1 className="text-center text-4xl md:text-7xl font-semibold">
-          MY GRIEVANCES
+          OFFICER DETAILS
         </h1>
         <section class="container mx-auto font-mono flex justify-center">
           <div class=" pt-4 mb-8 overflow-y-scroll overflow-x-scroll h-120  mt-4 border-2 shadow-2xl rounded-xl p-6 overflow-hidden w-11/12 md:w-5/6">
@@ -247,9 +132,9 @@ function checkLogin() {
               <table class="w-full">
                 <thead>
                   <tr class="text-md font-semibold tracking-wide text-left text-gray-900 bg-gray-100 uppercase border-b border-gray-600">
-                    <th class="px-4 py-3">Date</th>
+                    <th class="px-4 py-3">ID</th>
+                    <th class="px-4 py-3">Name</th>
                     <th class="px-4 py-3">Department</th>
-                    <th class="px-4 py-3">Grievance</th>
                     <th class="px-4 py-3">Status</th>
                     <th class="px-4 py-3">Reminder</th>
                     <th class="px-4 py-3 mx-auto">View Action History</th>

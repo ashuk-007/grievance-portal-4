0e236c7a8888889614fcc77bd1729fb79692360e
Priyadarshini <priyadarshini.roy03@gmail.com>
2023-05-03 13:23:20 +0530

    admin routes (register, delete, get)

--- server/populate.js
+++ server/populate.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const districts = require('./districts').districtArray
 var ObjectId = require('mongodb').ObjectID;
 
 const connectDB = require('./db/connect')
@@ -12,6 +13,20 @@ const OfficerRatings = require('./models/OfficerRatings')
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI)
+    for (const i in districts) {
+      email = districts[i].replace(/ /g, '').toLowerCase() + "admin@gmail.com";
+      adminName = districts[i] + " Admin";
+      password = "password"
+      distr = districts[i]
+
+      await Admin.create({
+        name: adminName,
+        email: email,
+        password: password,
+        district: distr,
+      })
+      console.log(adminName, email, password, distr)
+    }
     // await Admin.deleteMany()
     // await Admin.create(adminValues)
     // const officers = await Officer.find({ district: 'Gwalior' });
@@ -34,10 +49,10 @@ const start = async () => {
     // }
     // console.log("data created.");
 
-    await Complaint.deleteMany();
-    console.log("complaints deleted")
-    await OfficerRatings.updateMany({}, { avgRating: null, ratings: [] });
-    console.log("officer ratings updated")
+    // await Complaint.deleteMany();
+    // console.log("complaints deleted")
+    // await OfficerRatings.updateMany({}, { avgRating: null, ratings: [] });
+    // console.log("officer ratings updated")
     // await OfficerRatings.deleteMany()
     // await Officer.create(officerValues)
     // await Officer.updateMany(
